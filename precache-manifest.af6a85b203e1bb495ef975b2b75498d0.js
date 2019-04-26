5c3b17b34674ea24b78665594c659dc007086bd0
crcass <ccass@live.com>
2019-04-26 11:05:06 -0500

    Updates

---
+++ precache-manifest.af6a85b203e1bb495ef975b2b75498d0.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ca6bfc1e26f79b0355cc0f0bc5b644",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "dd4453c0db827dbcad0c",
+    "url": "/memory-game/static/js/2.fc7413d0.chunk.js"
+  },
+  {
+    "revision": "be7cbbc696bc5542a008",
+    "url": "/memory-game/static/js/main.da56b39d.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  },
+  {
+    "revision": "5e1c5f166cf58160f786712d6ee822d7",
+    "url": "/memory-game/static/media/arsenal.5e1c5f16.png"
+  },
+  {
+    "revision": "aed48d2293fdeea2db38aa60870ec933",
+    "url": "/memory-game/static/media/bournemouth.aed48d22.png"
+  },
+  {
+    "revision": "3940b7f03bb24cb1e47cc91811f6f893",
+    "url": "/memory-game/static/media/brighton.3940b7f0.png"
+  },
+  {
+    "revision": "ff6117d0c81f372843b17f3334003a0d",
+    "url": "/memory-game/static/media/burnley.ff6117d0.png"
+  },
+  {
+    "revision": "c2d06368ce7fa6e9ca49192580e120b8",
+    "url": "/memory-game/static/media/cardiff.c2d06368.png"
+  },
+  {
+    "revision": "85a3a9a74b80f57f9ee55daab5a73e2b",
+    "url": "/memory-game/static/media/chelsea.85a3a9a7.png"
+  },
+  {
+    "revision": "ed6e1e5bde050bfbfd4b5fae9d8f565f",
+    "url": "/memory-game/static/media/crystalpalace.ed6e1e5b.png"
+  },
+  {
+    "revision": "fa5c96f6a20b8c06a0970be5bf014db2",
+    "url": "/memory-game/static/media/everton.fa5c96f6.png"
+  },
+  {
+    "revision": "d4283988013426ebdbb9ea2f229dc208",
+    "url": "/memory-game/static/media/fulham.d4283988.png"
+  },
+  {
+    "revision": "dc8238efb09ee7b7649e27b3a61e021a",
+    "url": "/memory-game/static/media/hero_bg.dc8238ef.jpg"
+  },
+  {
+    "revision": "115549e9c96467cccf311ab375680e3d",
+    "url": "/memory-game/static/media/huddersfield.115549e9.png"
+  },
+  {
+    "revision": "d39359fb06cad713292d44f6d5cdeef9",
+    "url": "/memory-game/static/media/leicester.d39359fb.png"
+  },
+  {
+    "revision": "69f1b2396f2686ef15f13da448de303b",
+    "url": "/memory-game/static/media/liverpool.69f1b239.png"
+  },
+  {
+    "revision": "f32d00e40b0d65051880f24bc1dc94c7",
+    "url": "/memory-game/static/media/manchester_c.f32d00e4.png"
+  },
+  {
+    "revision": "ab988b025191038b25690327ef06951f",
+    "url": "/memory-game/static/media/manchester_u.ab988b02.png"
+  },
+  {
+    "revision": "9230d00bf0df7e878e129a39f744a5b2",
+    "url": "/memory-game/static/media/newcastle.9230d00b.png"
+  },
+  {
+    "revision": "fb2f0b0af16f68b49f5c4e7731539805",
+    "url": "/memory-game/static/media/southampton.fb2f0b0a.png"
+  },
+  {
+    "revision": "352f16fd77d58540399a631342ac4930",
+    "url": "/memory-game/static/media/tottenham.352f16fd.png"
+  },
+  {
+    "revision": "5d71189fc8ef404d33e46d2098f8552d",
+    "url": "/memory-game/static/media/watford.5d71189f.png"
+  },
+  {
+    "revision": "a923e7985d723f181540b9694d710ebe",
+    "url": "/memory-game/static/media/westham.a923e798.png"
+  },
+  {
+    "revision": "0985f94f451acc7fa4e5b7cf40511a56",
+    "url": "/memory-game/static/media/wolverhampton.0985f94f.png"
+  }
+]);
