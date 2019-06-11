407cada297b6e96139349a62ddb120a0844625b4
crcass <ccass@live.com>
2019-06-11 15:30:09 -0500

    update readme, update teams for 2019-2020

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 # EPL Memory Game
 
-## Overview
+![EPL Memory Game](/public/images/thumbnail.jpg)
+
+## Technology
 
 This app was built with [React](https://reactjs.org/) & [styled-components](https://www.styled-components.com/)
 
-The user tries to select as many teams as they can without choosing any team more than once. After each attempt, the images are shuffled and the user tries again. The user's score & high score are updated accordingly, and a message is displayed to show success or failure.
+## Concept
+
+A memory game where the user tries to select as many teams as they can without choosing any team more than once. After each attempt, the images are shuffled and the user tries again. The user's score & high score are updated accordingly, and a message is displayed to show success or failure.
+
+## Motivation
+
+This game was used as a way to improve my React skills and begin using styled-components. I thought using the teams in the [English Premier League](https://www.premierleague.com/) would provide a good challenge for anyone that plays the game due the to the number of teams and the similarity in many of the colors & logos.
+
+## Design Process
+
+I was provided an example of a finished product as a starting place, but all the code in this version is my own. To make the game feel more authentic, I chose to use the same color scheme as the Official EPL website. Each of the team logos is populated from an array of objects. As new teams are added/removed from the EPL each season, I can easily update this array of objects with the current teams.
+
+## Future Development
+
+- Add animations. I would like to add animations to the game to provide more feedback to the user when they get an answer correct or incorrect.
+
+## Known Issues
+
+- None known so far. As you discover issues, please report them!
 
 ♥︎ cc
--- src/Game/data/teams.js
+++ src/Game/data/teams.js
@@ -1,18 +1,21 @@
 import arsenal from './images/arsenal.png';
+import astonvilla from './images/astonvilla.png';
 import brighton from './images/brighton.png';
 import bournemouth from './images/bournemouth.png';
 import burnley from './images/burnley.png';
-import cardiff from './images/cardiff.png';
+// import cardiff from './images/cardiff.png';
 import chelsea from './images/chelsea.png';
 import crystalpalace from './images/crystalpalace.png';
 import everton from './images/everton.png';
-import fulham from './images/fulham.png';
-import huddersfield from './images/huddersfield.png';
+// import fulham from './images/fulham.png';
+// import huddersfield from './images/huddersfield.png';
 import leicester from './images/leicester.png';
 import liverpool from './images/liverpool.png';
 import manchester_c from './images/manchester_c.png';
 import manchester_u from './images/manchester_u.png';
 import newcastle from './images/newcastle.png';
+import norwich from './images/norwich.png';
+import sheffield from './images/sheffield.png';
 import southampton from './images/southampton.png';
 import tottenham from './images/tottenham.png';
 import watford from './images/watford.png';
@@ -21,20 +24,23 @@ import wolverhampton from './images/wolverhampton.png';
 
 const teams = [
   { name: 'Arsenal', image: arsenal, color: '#F00100' },
+  { name: 'Aston Villa', image: astonvilla, color: '#660133' },
   { name: 'Brighton', image: brighton, color: '#0153A6' },
   { name: 'Bournemouth', image: bournemouth, color: '#D71820' },
   { name: 'Burnley', image: burnley, color: '#81204C' },
-  { name: 'Cardiff', image: cardiff, color: '#1A365D' },
+  // { name: 'Cardiff', image: cardiff, color: '#1A365D' },
   { name: 'Chelsea', image: chelsea, color: '#0B4595' },
   { name: 'Crystal Palace', image: crystalpalace, color: '#004A9A' },
   { name: 'Everton', image: everton, color: '#00369C' },
-  { name: 'Fulham', image: fulham, color: '#F5F5F5' },
-  { name: 'Huddersfield', image: huddersfield, color: '#0072CE' },
+  // { name: 'Fulham', image: fulham, color: '#F5F5F5' },
+  // { name: 'Huddersfield', image: huddersfield, color: '#0072CE' },
   { name: 'Leicester', image: leicester, color: '#0D55A4' },
   { name: 'Liverpool', image: liverpool, color: '#D3181E' },
   { name: 'Manchester City', image: manchester_c, color: '#98C5E9' },
   { name: 'Manchester United', image: manchester_u, color: '#C70000' },
   { name: 'Newcastle', image: newcastle, color: '#383838' },
+  { name: 'Norwich', image: norwich, color: '#FEF204' },
+  { name: 'Sheffield', image: sheffield, color: '#BB0001' },
   { name: 'Southampton', image: southampton, color: '#E60225' },
   { name: 'Tottenham', image: tottenham, color: '#111836' },
   { name: 'Watford', image: watford, color: '#FBEE23' },
